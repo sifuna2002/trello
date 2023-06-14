dc8f18af449231916832aac13af0c2b1ba3f99d2
sifuna2002 <sifunadone4704@gmail.com>
2023-06-14 20:22:40 +0300

    zustad

---
+++ components/Column.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
+
+type Props={
+    id:TypedColumn,
+    todos:Todo[],
+    index:number;
+}
+const idToColumnText:{
+    [key in TypedColumn]:string
+}={
+    'todo':'To Do',
+    'inprogress':'In Progress',
+    'done':'Done'
+}
+function Column({id,todos,index}:Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+        {(provided) => (
+            <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            >
+                <Droppable droppableId={index.toString()} type='card'>
+                    {(provided,snapshot) => (
+                        <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className={`p-2 rounded-2xl shadow-sm ${
+                            snapshot.isDraggingOver ? 'bg-green-200' : 'bg-white/50'}
+                            
+                        }`}
+                        >
+                            <h2>{idToColumnText[id]}</h2>
+                        </div>
+                    )}
+                </Droppable>
+            </div>
+        )}
+    </Draggable>
+  )
+}
+
+export default Column
